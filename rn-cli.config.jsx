b7723823db8e8d46e94c02ad36dcb97be5556ee1
arjunnair7 <68246211+arjunisgoated@users.noreply.github.com>
2023-02-04 02:35:42 -0600

    Add files via upload

---
+++ rn-cli.config.jsx
@@ -0,0 +1,31 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+  getProjectRoots() {
+    return this._getRoots();
+  },
+
+  getAssetRoots() {
+    return this._getRoots();
+  },
+
+  getAssetExts() {
+    return ["obj", "mtl", "JPG", "vrx", "hdr", "gltf", "glb", "bin", "arobject"];
+  },
+
+  _getRoots() {
+    // match on either path separator
+    if (__dirname.match(/node_modules[\/\\]react-native[\/\\]packager$/)) {
+      // packager is running from node_modules of another project
+      return [path.resolve(__dirname, '../../..')];
+    } else if (__dirname.match(/Pods\/React\/packager$/)) {
+      // packager is running from node_modules of another project
+      return [path.resolve(__dirname, '../../..')];
+    } else {
+      return [path.resolve(__dirname, '.')];
+    }
+  },
+
+};
